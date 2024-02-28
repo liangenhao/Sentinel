b6e1146fef8a16e38695fad856c0f47ee612ca71
liangenhao <liangenhao@hotmail.com>
2024-02-28 16:27:50 +0800

    fix: Fix the issue of losing historical rules when adding/updating rules after restarting the dashboard.
    Due to the fact that Sentinel dashboard rule configurations are stored in memory, the rules are lost after restarting the service. The effects after fixing this issue are as follows:
    1. Rules with the same ID within the same type of rule will be filtered out, retaining the latter one.
    2. Rules with the same rule configuration (Rule object instance) within the same type of rule will be filtered out, retaining the latter one.

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/extension/datasource/nacos/NacosDynamicRuleProcessor.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/extension/datasource/nacos/NacosDynamicRuleProcessor.java
@@ -4,11 +4,13 @@ import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.RuleEntity;
 import com.alibaba.csp.sentinel.dashboard.extension.datasource.DynamicRuleProcessor;
 import com.alibaba.csp.sentinel.dashboard.extension.datasource.RuleType;
 import com.alibaba.csp.sentinel.dashboard.extension.datasource.codec.Codec;
+import com.alibaba.csp.sentinel.slots.block.Rule;
 import com.alibaba.csp.sentinel.util.StringUtil;
 import com.alibaba.nacos.api.config.ConfigService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Nacos implementation of {@link DynamicRuleProcessor}
@@ -47,9 +49,24 @@ public class NacosDynamicRuleProcessor<T extends RuleEntity> extends DynamicRule
         if (rules == null) {
             return;
         }
+
+        // 解决 sentinel 规则数据未持久化，导致服务重启后，推送规则导致历史规则数据丢失问题:
+        // 先从数据源中查询出数据，然后添加本次的数据，最后推送
+        // 存在问题：会把配置完全相同的规则给过滤掉
+        List<T> allRules = Optional.ofNullable(getRules(app)).orElseGet(ArrayList::new);
+        allRules.addAll(rules);
+        // 根据 RuleEntity#id 去重，保留后面的
+        allRules = new ArrayList<>(allRules.stream()
+                .collect(Collectors.toMap(RuleEntity::getId, Function.identity(), (e1, e2) -> e2, LinkedHashMap::new))
+                .values());
+        // 根据 Rule 对象去重，保留后面的
+        Map<Rule, T> ruleMap = allRules.stream()
+                .collect(Collectors.toMap(RuleEntity::toRule, Function.identity(), (e1, e2) -> e2, LinkedHashMap::new));
+        Collection<T> distinctRuleEntities = ruleMap.values();
+
         String dataId = getDataId(app, this.ruleType);
         String group = properties.getGroup();
-        String encodedContent = codec.encode(rules);
+        String encodedContent = codec.encode(distinctRuleEntities);
         boolean success = configService.publishConfig(dataId, group, encodedContent, codec.getFormat().getType());
         if (!success) {
             String errorMsg = String.format("Nacos publish config failed, dataId=%s, group=%s, content=%s, type=%s",
